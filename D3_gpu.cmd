1c553a89d4aebb8cf20f7fc217c2b1c53fb2af60
David Yi <davidyi6@patas.ling.washington.edu>
2022-05-08 16:20:52 -0700

    Adjust condor output file names

--- D3_gpu.cmd
+++ D3_gpu.cmd
@@ -1,8 +1,8 @@
 Universe = vanilla
 Executable = D3_run.sh
-Output = D3_pred.out
-Log = D3_pred.log
-Error = D3_pred.err
+Output = D3.out
+Log = D3.log
+Error = D3.err
 request_GPUs = 1
 request_memory = 8192
 Requirements = (Machine == "patas-gn1.ling.washington.edu" || Machine == "patas-gn2.ling.washington.edu")
