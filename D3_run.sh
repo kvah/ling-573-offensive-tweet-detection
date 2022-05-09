9bead5a16e43d79fe0e8203805dedf5cc91f4973
David Yi <davidyi6@patas.ling.washington.edu>
2022-05-08 17:21:39 -0700

    Fix D3_run.sh args

--- D3_run.sh
+++ D3_run.sh
@@ -3,22 +3,22 @@
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate ./573_gpu
 
-# # Preprocess tweets
-# python3 src/preprocess_olid.py \
-#     --file data/olid-training-v1.0.tsv \
-#     --train_ids data/train_ids.txt \
-#     --val_ids data/val_ids.txt \
-#     --split_punctuation \
-#     --remove_apostraphes \
-#     --remove_hashtags
+# Preprocess tweets
+python3 src/preprocess_olid.py \
+    --file data/olid-training-v1.0.tsv \
+    --train_ids data/train_ids.txt \
+    --val_ids data/val_ids.txt \
+    --split_punctuation \
+    --remove_apostraphes \
+    --remove_hashtags
 
-# # Train LSTM classifier
-# python3 src/lstm_train.py \
-#     --config configs/D3.json \
-#     --train_data data/clean_train_olid.tsv \
-#     --val_data data/clean_val_olid.tsv \
-#     --model_config_path lstm_saved_configs \
-#     --train_tokens_only 
+# Train LSTM classifier
+python3 src/lstm_train.py \
+    --config configs/D3.json \
+    --train_data data/clean_train_olid.tsv \
+    --val_data data/clean_val_olid.tsv \
+    --model_config_path lstm_saved_configs \
+    --train_tokens_only 
 
 # Run LSTM predictions and generate output
 python3 src/lstm_predict.py \
@@ -26,7 +26,7 @@ python3 src/lstm_predict.py \
     --train_data data/clean_train_olid.tsv \
     --val_data data/clean_val_olid.tsv \
     --model_config_path lstm_saved_configs \
-    --model_path models/lstm_epoch11.pt \
+    --model_path models/lstm_D3_best_model.pt \
     --fig_path outputs/D3_F1_curve.png \
     --val_output_csv outputs/D3/D3_val_preds.csv \
     --train_tokens_only
