4da672ef8a453fee993e6b3be02298a0dbaf6690
David Yi <davidyi6@patas.ling.washington.edu>
2022-05-18 12:16:26 -0700

    add config files

---
+++ D4.cmd
@@ -0,0 +1,9 @@
+Universe = vanilla
+Executable = finetune_pretrained.sh
+Output = finetune_xlmr_2.out
+Log = finetune_xlmr_2.log
+Error = finetune_xlmr_2.err
+request_memory = 8192
+request_GPUs = 1
+Requirements = (Machine == "patas-gn2.ling.washington.edu")
+Queue
