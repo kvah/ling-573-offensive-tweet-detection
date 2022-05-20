2d116d7566bafc3535af27c87b420189a983ff63
David Yi <davidyi6@patas.ling.washington.edu>
2022-05-19 22:35:05 -0700

    Dynamically create output files based on input config name

--- D4.cmd
+++ D4.cmd
@@ -1,8 +1,10 @@
+Config = finetune_mbert
 Universe = vanilla
 Executable = finetune_pretrained.sh
-Output = finetune_xlmr_2.out
-Log = finetune_xlmr_2.log
-Error = finetune_xlmr_2.err
+Output = condor_output/$(Config).out
+Log = condor_output/$(Config).log
+Error = condor_output/$(Config).err
+arguments = $(Config)
 request_memory = 8192
 request_GPUs = 1
 Requirements = (Machine == "patas-gn2.ling.washington.edu")
--- finetune_pretrained.sh
+++ finetune_pretrained.sh
@@ -1,22 +1,34 @@
 #!/bin/sh
 
+# A script to finetune a pretrained huggingface model and eval on OLID data
+# Note: Set Config parameter in D4.cmd
+
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate ./573_gpu
 
 # Test whether condor is using GPU
 python3 src/test_gpu.py
 
+# Preprocess tweets
+python3 src/preprocess_olid.py \
+    --file data/olid-training-v1.0.tsv \
+    --train_ids data/train_ids.txt \
+    --val_ids data/val_ids.txt \
+    --split_punctuation \
+    --remove_apostraphes \
+    --remove_hashtags
+
 # Finetune pretrained model on training data
 python3 src/finetune_pretrained.py \
     --train_data data/clean_train_olid.tsv \
     --val_data data/clean_val_olid.tsv \
-    --config configs/finetune_xlmr_2.json
+    --config configs/${1}.json
 
 # Run finetuned model predictions and generate output
 python3 src/finetune_predict.py \
     --val_data data/clean_val_olid.tsv \
-    --config configs/finetune_xlmr.json \
-    --model_path models/finetune_xlmr_2 \
+    --config configs/${1}.json \
+    --model_path models/${1} \
     --val_output_csv outputs/D4/D4_val_preds.csv
 
 # Evaluation script
